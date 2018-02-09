d42a818ffda13d3c8965ddabe4f8a93efcf3f84a
Redocram <redocram@gmail.com>
2018-02-09 16:06:10 +0100

    Installe EF and prepared gitignore for connectionstring

--- SportsStore.Domain/Concrete/EFDbContext.cs
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -1,19 +1,21 @@
 ﻿// compile with: /doc:SportStoreDoc.xml 
 
 using SportsStore.Domain.Entities;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Text;
 
 namespace SportsStore.Domain.Concrete
 {
+    /// <summary>
+    /// This class associate the models with the db</summary>
     public class EFDbContext : DbContext
     {
         /// <summary>
         /// This property is connected to the Products table.</summary>
         /// <value>The type parameter of the DbSet result specifies the model type that the EF should use to represent row in that table</value>
         public DbSet<Product> Products { get; set; }
+        /// <summary>
+        /// This property is connected to the Categories table.</summary>
+        /// <value>The type parameter of the DbSet result specifies the model type that the EF should use to represent row in that table</value>
         public DbSet<Category> Categories { get; set; }
 
     }
