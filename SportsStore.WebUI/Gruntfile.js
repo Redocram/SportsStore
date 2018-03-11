8a93f5f866d717ff5cffed40085e2b2efc2634c0
Redocram <redocram@gmail.com>
2018-03-11 16:02:33 +0100

    added browswer sync to project

--- SportsStore.WebUI/Gruntfile.js
+++ SportsStore.WebUI/Gruntfile.js
@@ -2,28 +2,44 @@
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {                              // Task 
-      dist: {                            // Target 
-        options: {                       // Target options 
-          style: 'expanded'
-        },
-        files: {                         // Dictionary of files 
-          './Content/CustomContent/CustomStyle.css': './Content/CustomContent/CustomStyle.scss'       // 'destination': 'source' 
-        }
-      }
-    },
+    pkg: grunt.file.readJSON('package.json'),   
     watch: {
         files: ['Content/CustomContent/CustomStyle.scss'],
         tasks: ['sass'],
         options: {
           spawn: false
         }
+    },
+    sass: {                              // Task 
+        dist: {                            // Target 
+            options: {                       // Target options 
+                style: 'expanded'
+            },
+            files: {                         // Dictionary of files 
+                './Content/CustomContent/CustomStyle.css': './Content/CustomContent/CustomStyle.scss'       // 'destination': 'source' 
+            }
+        }
+    },
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src: [
+                    './Content/CustomContent/CustomStyle.css',
+                    '/Views/**/*.cshtml'
+                ]
+            },
+            options: {
+                watchTask: true,
+                proxy: 'localhost:52617',
+                tunnel: true
+            }
+        }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['browserSync', 'watch']);
 };
